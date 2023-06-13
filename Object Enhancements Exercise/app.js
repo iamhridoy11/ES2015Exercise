c9ec4e836d298f1ca952f715fedfa0f43483e3b7
iamhridoy11 <m_rahman_hridoy@yahoo.com>
2023-06-12 20:24:55 -0400

    Object Enhancements Exercise add

---
+++ Object Enhancements Exercise/app.js
@@ -0,0 +1,61 @@
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+// --------------------------------------------------------------------------------------
+// Same keys and values ES2015
+function createInstructor(firstName, lastName){
+    return{
+        firstName,
+        lastName
+    };
+};
+
+// -----------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+// ------------------------------------------------------------------------------------------
+// Computed Property Names ES2015
+
+let favoriteNumber = 42;
+
+const instructor = {
+    firstName : "Colt",
+    [favoriteNumber] : "This is my favorite!"
+};
+// -------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+// -------------------------------------------------------------------------------------------
+// ----------------------------------Object Methods ES2015---------------------------------------------------------
+
+const instructors ={
+    firstName : "Colt",
+    sayHi() {
+        return "Hi!";
+    },
+    sayBye() {
+        return this.firstName + "says bye";
+    }
+
+}
